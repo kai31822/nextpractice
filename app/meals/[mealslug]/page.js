01f6da19cd07067250fe6569723aba51e7fcfcbc
kai <kindkai624@gmail.com>
2024-01-03 21:26:14 +0800

    details page

--- app/meals/[mealslug]/page.js
+++ app/meals/[mealslug]/page.js
@@ -1,7 +1,35 @@
 import React from 'react';
-
+import classes from './page.module.css';
+import Image from 'next/image';
+import { getMeal } from '@/lib/meals';
 const MealDetailsPage = ({ params }) => {
-    return <div>Meal Details : {params.mealslug}</div>;
+    const meal = getMeal(params.mealslug);
+    /*
+    /\n/g  :It is a regular expression , which matches all the \n
+      */
+    meal.instructions = meal.instructions.replace(/\n/g, '<br />');
+
+    return (
+        <>
+            <header className={classes.header}>
+                {/* image */}
+                <div className={classes.image}>
+                    <Image src={meal.image} alt={meal.title} fill />
+                </div>
+                {/*  */}
+                <div className={classes.headerText}>
+                    <h1>{meal.title}</h1>
+                    <p className={classes.creator}>
+                        by <a href={`mailto:${meal.creator_mail}`}></a> {meal.creator}
+                    </p>
+                    <p className={classes.summary}>{meal.summary}</p>
+                </div>
+            </header>
+            <main>
+                <p className={classes.instructions} dangerouslySetInnerHTML={{ __html: meal.instructions }}></p>
+            </main>
+        </>
+    );
 };
 
 export default MealDetailsPage;
