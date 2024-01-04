1be2fc7d6227feaf39049f9087ca09e3d3a5afdf
kai <kindkai624@gmail.com>
2024-01-04 21:38:16 +0800

    表單  頁面中用use server版本

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,7 +1,62 @@
-import React from 'react';
+import classes from './page.module.css';
+import ImagePicker from '@/app/components/meals/image-picker';
 
-const SharePage = () => {
-    return <div>Share Meal</div>;
-};
+export default function ShareMealPage() {
+    //server action for form submission
+    async function shareMeal(formData) {
+        'use server';
+        const meal = {
+            tilte: formData.get('title'),
+            summatry: formData.get('summary'),
+            instructions: formData.get('instructions'),
+            image: formData.get('image'),
+            creator: formData.get('name'),
+            creator_email: formData.get('email'),
+        };
+        // test
+        console.log(meal);
+    }
 
-export default SharePage;
+    return (
+        <>
+            <header className={classes.header}>
+                <h1>
+                    Share your <span className={classes.highlight}>favorite meal</span>
+                </h1>
+                <p>Or any other meal you feel needs sharing!</p>
+            </header>
+            <main className={classes.main}>
+                {/* from */}
+                <form className={classes.form} action={shareMeal}>
+                    <div className={classes.row}>
+                        <p>
+                            <label htmlFor='name'>Your name</label>
+                            <input type='text' id='name' name='name' required />
+                        </p>
+                        <p>
+                            <label htmlFor='email'>Your email</label>
+                            <input type='email' id='email' name='email' required />
+                        </p>
+                    </div>
+                    <p>
+                        <label htmlFor='title'>Title</label>
+                        <input type='text' id='title' name='title' required />
+                    </p>
+                    <p>
+                        <label htmlFor='summary'>Short Summary</label>
+                        <input type='text' id='summary' name='summary' required />
+                    </p>
+                    <p>
+                        <label htmlFor='instructions'>Instructions</label>
+                        <textarea id='instructions' name='instructions' rows='10' required></textarea>
+                    </p>
+                    {/* IMAGE PICKER */}
+                    <ImagePicker label='your image' name='image'></ImagePicker>
+                    <p className={classes.actions}>
+                        <button type='submit'>Share Meal</button>
+                    </p>
+                </form>
+            </main>
+        </>
+    );
+}
