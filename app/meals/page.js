cdf6b5be8f3300f01f8c60a9b410fcce319132b1
kai <kindkai624@gmail.com>
2024-01-05 22:18:04 +0800

    matadata

--- app/meals/page.js
+++ app/meals/page.js
@@ -4,6 +4,11 @@ import Link from 'next/link';
 import MealsGrid from '@/app/components/meals/meals-grid';
 import { getMeals } from '@/lib/meals';
 
+export const metadata = {
+    title: 'All Meals',
+    description: 'Browse the delicious meals  shared by our vibrant community.',
+};
+
 async function Meals() {
     const meals = await getMeals();
     return <MealsGrid meals={meals}></MealsGrid>;
